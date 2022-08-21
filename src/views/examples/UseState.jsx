b9d72a16475c0d793df8ad4204fa2998c412a137
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-20 23:15:58 -0300

    useState #02

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -4,6 +4,8 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
     const [count, setCount ] = useState(0)
+    const [name, setName ] = useState('')
+    
     return (
         <div className="UseState">
             <PageTitle
@@ -22,6 +24,9 @@ const UseState = (props) => {
             
             </div>
             <SectionTitle title="Exercício #02" />
+            <input type="text" className="input" 
+                value={name} onChange={e => setName(e.target.value)}
+            />
              
         </div>
     )
